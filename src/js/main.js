24efd3c83fb46168e3709f8317cf166326402e88
Kwasiuu <z391702@gmail.com>
2022-11-12 15:41:07 +0100

    better validation

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,23 @@ const cardName = document.querySelector('#cardholder')
 const cardNumber = document.querySelector('#cardnumber')
 const month = document.querySelector('#mm')
 const year = document.querySelector('#yy')
+const dateTwoInput = document.querySelector('.form__two-input-date')
 const cvc = document.querySelector('#cvc')
+
 
 const warnCardholder = document.querySelector('.form__warn-card-cardholder')
 const warnCardNumber = document.querySelector('.form__warn-card-number')
 const warnDate = document.querySelector('.form__warn-date')
 const warnCvc = document.querySelector('.form__warn-cvc')
+const warnForm = document.querySelector('.form-warn')
 
 const confirmButton = document.querySelector('.form__button')
 const completedButton = document.querySelector('.completed__button')
 
 const completed = document.querySelector('.completed')
 
-
+const borderColorRed = '1px solid hsl(0, 100%, 66%)';
+const borderColorGrey = '1px solid hsl(270, 3%, 87%)';
 
 const fillName = () => {
     nameOnCard.textContent = cardName.value; 
@@ -43,12 +47,10 @@ const fillYear = () => {
 
     }
 }
-
 const fillCvc = () => {
     cvcOnCard.textContent = cvc.value; 
 
 }
-
 const addSpace = () => {
     if (cardNumber.value.length == 4 || cardNumber.value.length == 9 || cardNumber.value.length == 14) {
         cardNumber.value = cardNumber.value + ' ';
@@ -56,109 +58,90 @@ const addSpace = () => {
 }
 
 
-const validateName = () => {
-    if (cardName.value == '') {
-        warnCardholder.classList.remove('hide');
-        cardName.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        warnCardholder.classList.add('hide');
-        cardName.style.border = '1px solid hsl(270, 3%, 87%)';
-        validateNumber();
-    }
 
+const showError = (element, msg) => {
+    element.nextElementSibling.classList.add('hide')
+    element.style.border = borderColorRed
+    element.nextElementSibling.textContent = msg
 }
 
-const validateNumber = () => {
-    let regNum = /^[0-9]+$/;
-
-    if (cardNumber.value == '') {
-        warnCardNumber.classList.remove('hide');
-        cardNumber.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else if (cardNumber.value.match(regNum) || cardNumber.value.length != 19) {
-        warnCardNumber.classList.remove('hide');
-        warnCardNumber.textContent = 'Wrong format';
-        cardNumber.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        warnCardNumber.classList.add('hide');
-        cardNumber.style.border = '1px solid hsl(270, 3%, 87%)';
-        validateMonth();
-    }
+const clearError = (element) => {
+    element.nextElementSibling.classList.remove('hide')
+    element.style.border = borderColorGrey
 }
 
-const validateMonth = () => {
-    if (month.value == '') {
-        warnDate.classList.remove('hide');
-        month.style.border = '1px solid hsl(0, 100%, 66%)';
-
-    } else if (month.value > 12) {
-        warnDate.classList.remove('hide');
-        warnDate.textContent = 'Wrong format';
-        month.style.border = '1px solid hsl(0, 100%, 66%)';
-    } else {
-        warnDate.classList.add('hide');
-        month.style.border = '1px solid hsl(270, 3%, 87%)';
-        validateYear();
-    }
+const checkLength = (input, min) => {
+    
+    if(input.value.length < min) {
+        showError(input, 'Wrong format')
+    } 
 }
 
-const validateYear = () => {
-    let currentData = new Date().getFullYear();
 
-    if (year.value == '') {
-        warnDate.classList.remove('hide');
-        year.style.border = '1px solid hsl(0, 100%, 66%)';
+const checkForm = input => {
+    input.forEach(el => {
+        if (el.value === '') {
+            showError(el, 'Can t be blank!')
+        } else {
+            clearError(el)
+        }
+    })
+}
 
-    } else if (year.value < currentData-2000) {
-        warnDate.classList.remove('hide');
-        warnDate.textContent = 'Wrong format';
-        year.style.border = '1px solid hsl(0, 100%, 66%)';
-    } else {
-        warnDate.classList.add('hide');
-        year.style.border = '1px solid hsl(270, 3%, 87%)';
-        validateCvc();
+const reNum = /[^0-9]/igm;
+const reMonth = /^0[1-9]|1[0-2]$/;
+const reYear = /^[2-9][2-9]$/;
+
+const checkRegEx = (input, reg) => {
+    
+    if(reg.test(input.value)) {
+        clearError(input);
+    } else { 
+        showError(input, "Wrong format");
+    }
+    
+}
+
+const checkerrors = () => {
+    const allInputs = document.querySelectorAll('.form__warn')
+    let errorCount = 0
+
+    allInputs.forEach(el => {
+        if(el.classList.contains('hide')) {
+            errorCount++;
+        }
+    })
+    if(errorCount === 0 ) {
+        completed.classList.add('show-popup')
     }
 }
 
-const validateCvc = () => {
-
-    if (cvc.value == '') {
-        warnCvc.classList.remove('hide');
-        cvc.style.border = '1px solid hsl(0, 100%, 66%)';
-
-    } else if (cvc.value < 100 || cvc.value > 999) {
-        warnCvc.classList.remove('hide');
-        warnCvc.textContent = 'Wrong format';
-        cvc.style.border = '1px solid hsl(0, 100%, 66%)';
-    } else {
-        warnCvc.classList.add('hide');
-        cvc.style.border = '1px solid hsl(270, 3%, 87%)';
-        allCompleted();
-    }
-}
-
-const allCompleted = () => {
-    completed.classList.remove('hide-popup');
-}
 
 const resetPopup = () => {
-    completed.classList.add('hide-popup');
+    completed.classList.remove('show-popup');
     nameOnCard.textContent = 'Jane Appleseed';
     numberOnCard.textContent = '0000 0000 0000 0000';
     cvcOnCard.textContent = '000';
     monthOnCard.textContent = '00';
     yearOnCard.textContent = '00';
-
-    cardName.value = '';
-    cardNumber.value = '';
-    month.value = '';
-    year.value = '';
-    cvc.value = '';
+    [cardName, cardNumber, month, year, cvc].forEach(element => {
+        element.value = ''
+    })
 }
 
+confirmButton.addEventListener('click', e => {
+    e.preventDefault();
 
+    checkForm([cardName, cardNumber, month, year, cvc]);
+    checkLength(cardNumber, 16);
+    checkLength(cvc, 3)
+    checkRegEx(month, reMonth);
+    checkRegEx(year, reYear);
+    checkRegEx(cardNumber, reNum);
+    checkerrors();
+})
 
 completedButton.addEventListener('click', resetPopup)
-confirmButton.addEventListener('click', validateName)
 cardNumber.addEventListener('keydown', addSpace)
 cardName.addEventListener('keyup', fillName)
 cardNumber.addEventListener('keyup', fillNumber)
